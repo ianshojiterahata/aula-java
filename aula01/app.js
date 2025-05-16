082476706c32546b5d57889d3cac88d4e3bf6f0c
Ian Shoji Terahata <terahataian8@gmail.com>
2025-05-15 22:40:17 -0300

    fix: novo

--- aula01/app.js
+++ aula01/app.js
@@ -17,7 +17,8 @@ const inputEmail = document.getElementById('email')
 const inputAssunto = document.getElementById('assunto')
 const inputMensagem = document.getElementById('mensagem')
 const btnEnviar = document.getElementById('btnEnviar')
-const mensagemErro= document.getElementById('erro')
+const mensagemErro= document.getElementById('mensagemErro')
+const cadastro = document.getElementById('cadastro')
 
 btnEnviar.addEventListener("click", function (event){
     
@@ -34,7 +35,16 @@ btnEnviar.addEventListener("click", function (event){
     }
     alert(`Nome: ${nome}\n E-mail: ${email}\n assunto: ${assunto}\n Mensagem : ${mensagem}\n`)
 
+    const cardusuario = document.createElement("div")
+    cardusuario.innerHTML = `
+    <h3> Nome: ${nome} </h3>
+    <h3> E=mail: ${email} </h3>
+    <h3> Assunto: ${assunto} </h3>
+    <h3> Mensagem: ${mensagem} </h3>
+    `
 
+    cadastro.append(cardusuario)
+    form.reset()
 } ) 
 
 
