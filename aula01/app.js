4d639f15741891c769f1d001c5640bd3d3c68863
Ian Shoji Terahata <terahataian8@gmail.com>
2025-05-15 22:25:06 -0300

    feat novo

---
+++ aula01/app.js
@@ -0,0 +1,40 @@
+// ex01
+
+// const btnClicar =  document.getElementById("btnClicar")
+// const paragrafo = document.getElementById("paragrafo")
+
+// btnClicar.addEventListener("click", function () {
+//     paragrafo.textContent = "matheus bidu"
+
+// })
+
+
+// ex02
+
+const form = document.getElementById('form')
+const inputNome = document.getElementById('nome')
+const inputEmail = document.getElementById('email')
+const inputAssunto = document.getElementById('assunto')
+const inputMensagem = document.getElementById('mensagem')
+const btnEnviar = document.getElementById('btnEnviar')
+
+btnEnviar.addEventListener("click", function (event){
+    
+    event.preventDefault()
+    let nome = inputNome.Value
+    let email = inputEmail.Value
+    let assunto = inputAssunto.Value
+    let mensagem = inputMensagem.Value
+
+    if (nome === "" || email === "" ||assunto === ""  || mensagem === "") {
+        alert("insira algo valido")
+        return
+    }
+    alert(`Nome: ${nome}\n E-mail: ${email}\n assunto: ${assunto}\n Mensagem : ${mensagem}\n`)
+
+
+} ) 
+
+
+ 
+///////////////////////////////////////////////////////////////
